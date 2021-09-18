e350ac6763723082365692aa1f6f9c0852a63a2f
andrew-morozov <89417333+andrew-morozov@users.noreply.github.com>
2021-09-18 18:46:02 -0400

    Used Vue.js to Add Updating Data to UserInfo Page

---
+++ app/public/js/userInfo.js
@@ -0,0 +1,25 @@
+const Offer = {
+    data() {
+      return {
+        "person": {},
+        }
+    },
+    created() {
+        console.log("A");
+
+        fetch('https://randomuser.me/api/')
+        .then(response => response.json())
+        .then((parsedJson) => {
+            console.log(parsedJson);
+            this.person = parsedJson.results[0]
+            console.log("C");
+        })
+        .catch( err => {
+            console.error(err)
+        })
+
+        console.log("B");
+    }
+  }
+  
+Vue.createApp(Offer).mount('#table');
